a0a793274016ee4c4c03c6b21bd7fc832255b5b3
Daniel Yohan <dyohan9@gmail.com>
2020-12-03 10:47:30 -0300

    Added drf-yasg library for documentation

--- connect/urls.py
+++ connect/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg2 import openapi
+from drf_yasg2.views import get_schema_view
+from rest_framework import permissions
+
 from connect.api.v1 import urls as rookly_api_v1_urls
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1.0.0",
+        license=openapi.License(name="GPL-3.0 License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path("", schema_view.with_ui("redoc")),
     path('admin/', admin.site.urls),
     path("v1/", include(rookly_api_v1_urls)),
 ]
