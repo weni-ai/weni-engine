1f2717b16642c65f39f2f0c3d4920e7c74c34470
Daniel Yohan <dyohan9@gmail.com>
2020-12-17 18:04:48 -0300

    Added test dashboard

--- connect/settings.py
+++ connect/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
+
 import environ
 
 environ.Env.read_env(env_file=(environ.Path(__file__) - 2)(".env"))
@@ -40,6 +42,8 @@ DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
+TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
+
 
 # Application definition
 
@@ -51,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "drf_yasg2",
     "django_filters",
     "mozilla_django_oidc",
@@ -140,12 +145,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "mozilla_django_oidc.contrib.drf.OIDCAuthentication"
+        "mozilla_django_oidc.contrib.drf.OIDCAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
+
+if TESTING:
+    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append(
+        "rest_framework.authentication.TokenAuthentication"
+    )
 
 # mozilla-django-oidc
 OIDC_RP_CLIENT_ID = env.str("OIDC_RP_CLIENT_ID")
