5bfc1434177419a490ab22ab9691acebc4e5f06d
Daniel Yohan <dyohan9@gmail.com>
2020-12-17 17:00:36 -0300

    Added tests and coveralls

--- connect/common/tests.py
+++ connect/common/tests.py
@@ -1,1 +1,43 @@
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from connect.common.models import Newsletter, Service, ServiceStatus
+
+
+class NewsletterTestCase(TestCase):
+    def test_newsletter_create(self):
+        title = "New feature"
+        description = "test description"
+        newsletter = Newsletter.objects.create(title=title, description=description)
+        self.assertEqual(newsletter.title, title)
+        self.assertEqual(newsletter.description, description)
+
+
+class ServiceStatusTestCase(TestCase):
+    def setUp(self):
+        self.service = Service.objects.create(
+            url="http://test.com", status=False, default=False
+        )
+
+    def test_create_service_status(self):
+        user = User.objects.create_user("fake@user.com", "user", "123456")
+        status = ServiceStatus.objects.create(
+            service=self.service,
+            user=user,
+        )
+        self.assertEqual(status.service.url, "http://test.com")
+        self.assertEqual(status.service.status, False)
+        self.assertEqual(status.service.default, False)
+
+    def test_create_service_default(self):
+        service = Service.objects.create(
+            url="http://test-default.com", status=True, default=True
+        )
+        user = User.objects.create_user("fake@user.com", "user", "123456")
+        self.assertEqual(user.service_status.all().first().service.url, service.url)
+        self.assertEqual(
+            user.service_status.all().first().service.status, service.status
+        )
+        self.assertEqual(
+            user.service_status.all().first().service.default, service.default
+        )
