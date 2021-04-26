1bd07bca260ef0bda059fb009c83d8871c499346
Daniel Yohan <dyohan9@gmail.com>
2021-04-26 10:47:08 -0300

    Fix grpc type inteligence

--- weni/grpc/types/inteligence/type.py
+++ weni/grpc/types/inteligence/type.py
@@ -138,6 +138,6 @@ class InteligenceType(GRPCType):
     def get_organization_statistic(self, organization_id: str):
         stub = organization_pb2_grpc.OrgControllerStub(self.channel)
         response = stub.Retrieve(
-            organization_pb2.OrgStatisticRetrieveRequest(org_iid=organization_id)
+            organization_pb2.OrgStatisticRetrieveRequest(org_id=organization_id)
         )
         return {"repositories_count": response.repositories_count}
