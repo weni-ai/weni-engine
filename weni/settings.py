f5f1edbf38ac539532d9298ded0f9ab4228909d3
Daniel Yohan <dyohan9@gmail.com>
2021-06-08 16:19:00 -0300

    Added scope settings

--- weni/settings.py
+++ weni/settings.py
@@ -315,6 +315,7 @@ OIDC_DRF_AUTH_BACKEND = env.str(
     "OIDC_DRF_AUTH_BACKEND",
     default="weni.oidc_authentication.WeniOIDCAuthenticationBackend",
 )
+OIDC_RP_SCOPES = env.str("OIDC_RP_SCOPES", default='openid email')
 
 # Swagger
 
