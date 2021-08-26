071a15639b77baa5b6596f2d99cfc4a74e055c59
Daniel Yohan <dyohan9@gmail.com>
2021-08-26 13:02:21 -0300

    Updated django admin models

--- weni/common/admin.py
+++ weni/common/admin.py
@@ -7,46 +7,61 @@ from weni.common.models import (
     Organization,
     Project,
     OrganizationAuthorization,
-    LogService,
     NewsletterLanguage,
+    BillingPlan,
 )
 
 
-@admin.register(NewsletterLanguage)
-class NewsletterLanguageAdmin(admin.ModelAdmin):
-    pass
+class BillingPlanInline(admin.TabularInline):
+    model = BillingPlan
+    extra = 1
+    min_num = 1
+    readonly_fields = [
+        "payment_method",
+        "last_invoice_date",
+        "next_due_date",
+        "termination_date",
+        "stripe_configured_card",
+        "final_card_number",
+        "card_expiration_date",
+        "cardholder_name",
+        "card_brand",
+    ]
+    can_delete = False
 
 
-@admin.register(Newsletter)
-class NewsletterAdmin(admin.ModelAdmin):
-    pass
+class OrganizationAuthorizationInline(admin.TabularInline):
+    model = OrganizationAuthorization
+    extra = 1
+    min_num = 1
+    can_delete = False
 
 
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(ServiceStatus)
-class ServiceStatusAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
-    pass
+    inlines = [BillingPlanInline, OrganizationAuthorizationInline]
+    search_fields = ["name", "inteligence_organization"]
 
 
-@admin.register(Project)
+class ServiceStatusInline(admin.TabularInline):
+    model = ServiceStatus
+
+
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ServiceStatusInline]
 
 
-@admin.register(OrganizationAuthorization)
-class OrganizationAuthorizationAdmin(admin.ModelAdmin):
-    pass
+class NewsletterLanguageInline(admin.TabularInline):
+    model = NewsletterLanguage
+    min_num = 2
+    can_delete = False
 
 
-@admin.register(LogService)
-class LogServiceAdmin(admin.ModelAdmin):
-    pass
+class NewsletterAdmin(admin.ModelAdmin):
+    inlines = [NewsletterLanguageInline]
+
+
+admin.site.register(Newsletter, NewsletterAdmin)
+admin.site.register(Service)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+
