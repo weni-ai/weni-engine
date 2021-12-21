e975402cd8ec5b4327c92787651ea8ac3ece18cc
Jackson Barbosa <jbs@ic.ufal.br>
2021-12-21 10:14:58 -0300

    get flow instance

--- connect/api/v1/invoice/views.py
+++ connect/api/v1/invoice/views.py
@@ -1,13 +1,17 @@
+from botocore.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
 
 from connect.api.v1.invoice.filters import InvoiceFilter
 from connect.api.v1.invoice.serializers import InvoiceSerializer
 from connect.api.v1.metadata import Metadata
-from connect.common.models import Invoice
+from connect.common.models import Invoice, Organization
+from django.utils.translation import ugettext_lazy as _
 
 
 class InvoiceViewSet(
@@ -26,10 +30,22 @@ class InvoiceViewSet(
     @action(
         detail=True,
         methods=["GET"],
-        url_name="remove-card-setup",
-        url_path="remove-card-setup/(?P<organization_uuid>[^/.]+)",
+        url_name="invoice-data",
+        url_path="invoice-data/(?P<organization_uuid>[^/.]+)",
     )
-    def remove_card_setup(
+    def invoice_data(
             self, request, organization_uuid
     ):
-        pass
+        organization = get_object_or_404(Organization, uuid=organization_uuid)
+
+        self.check_object_permissions(self.request, organization)
+
+        after = str(request.query_params.get('after') + "00:01")
+        before = str(request.query_params.get('before') + "23:59")
+
+        if (not before) or (not after):
+            raise ValidationError(
+                _("Need to pass 'before' and 'after' in query params")
+            )
+
+        flow_instance = utils.get_grpc_types().get("flow")
