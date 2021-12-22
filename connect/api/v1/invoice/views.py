c4be10e5f157f2f0b55ecb8e500ca2b85d03a843
Jackson Barbosa <jbs@ic.ufal.br>
2021-12-22 18:10:08 -0300

    add endpoint to get invoice data

--- connect/api/v1/invoice/views.py
+++ connect/api/v1/invoice/views.py
@@ -1,17 +1,21 @@
-from botocore.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import mixins
+from rest_framework import mixins, status
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.decorators import action
 
+from django.utils import timezone
+
 from connect.api.v1.invoice.filters import InvoiceFilter
 from connect.api.v1.invoice.serializers import InvoiceSerializer
 from connect.api.v1.metadata import Metadata
-from connect.common.models import Invoice, Organization
-from django.utils.translation import ugettext_lazy as _
+from connect.common.models import Invoice, Organization, BillingPlan
+from connect import utils
+from connect.billing.gateways.stripe_gateway import StripeGateway
+
+from django.http import JsonResponse
 
 
 class InvoiceViewSet(
@@ -25,6 +29,7 @@ class InvoiceViewSet(
     filter_class = InvoiceFilter
     filter_backends = [OrderingFilter, SearchFilter, DjangoFilterBackend]
     lookup_field = "pk"
+
     metadata_class = Metadata
 
     @action(
@@ -37,15 +42,39 @@ class InvoiceViewSet(
             self, request, organization_uuid
     ):
         organization = get_object_or_404(Organization, uuid=organization_uuid)
-
         self.check_object_permissions(self.request, organization)
-
-        after = str(request.query_params.get('after') + "00:01")
-        before = str(request.query_params.get('before') + "23:59")
-
-        if (not before) or (not after):
-            raise ValidationError(
-                _("Need to pass 'before' and 'after' in query params")
-            )
-
+        invoice_id = request.query_params.get("invoice_id")
+        invoice = get_object_or_404(organization.organization_billing_invoice, invoice_random_id=invoice_id)
         flow_instance = utils.get_grpc_types().get("flow")
+        invoice_data = {
+            "billing_date": invoice.due_date,
+            "invoice_date": timezone.now().strftime("%Y-%m-%d"),
+            "plan": organization.organization_billing.plan,
+            "invoice_id": invoice.invoice_random_id,
+            'total_invoice_amount': invoice.total_invoice_amount
+        }
+        before = str(request.query_params.get("before") + " 00:00")
+        after = str(request.query_params.get("after") + " 00:00")
+        payment_data = {
+            'payment_method': invoice.payment_method,
+            'projects': [],
+        }
+        for project in organization.project.all():
+            contact_count = flow_instance.get_billing_total_statistics(
+                project_uuid=str(project.flow_organization),
+                before=before,
+                after=after,
+            ).get("active_contacts")
+            payment_data['projects'].append(
+                {
+                    'project_name': project.name,
+                    'contact_count': contact_count,
+                    'price': BillingPlan.calculate_amount(contact_count)
+                }
+            )
+            client_data = StripeGateway().get_user_detail_data(organization.organization_billing.stripe_customer)
+        return JsonResponse(data={
+            "payment_data": payment_data,
+            "invoice": invoice_data,
+            "client_data": client_data
+        }, status=status.HTTP_200_OK)
