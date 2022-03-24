2691f66e3588159099403c43073b4510eeb1bc98
Alisson <alissonfilipe937@gmail.com>
2022-03-24 21:01:20 +0000

    card verification charge

--- connect/api/v1/organization/serializers.py
+++ connect/api/v1/organization/serializers.py
@@ -35,6 +35,7 @@ class BillingPlanSerializer(serializers.ModelSerializer):
             "problem_capture_invoice",
             "currenty_invoice",
             "contract_on",
+            "is_card_valid"
         ]
         ref_name = None
 
