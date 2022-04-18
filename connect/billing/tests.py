b4476f6a00b864647ed90482c5506a11df360a07
Alisson <alissonfilipe937@gmail.com>
2022-04-18 19:09:30 -0300

    Channel tests

--- connect/billing/tests.py
+++ connect/billing/tests.py
@@ -1,9 +1,14 @@
+import uuid
+import stripe
+
 from unittest import skipIf
 
 from django.test import TestCase
 from connect.billing import get_gateway
-import stripe
+
 from django.conf import settings
+from connect.billing.models import Channel
+from connect.common.models import Organization, BillingPlan
 
 
 @skipIf(not settings.BILLING_SETTINGS.get("stripe", None), "gateway not configured")
@@ -47,3 +52,37 @@ class StripeGatewayTestCase(TestCase):
     def test_get_payment_method_details_fail(self):
         resp = self.merchant.get_payment_method_details("ch_3K9wZYGB60zUb40p1C0iisk")
         self.assertEquals(resp['status'], 'FAIL')
+
+
+class ChannelTestCase(TestCase):
+    def setUp(self):
+        self.organization = Organization.objects.create(
+            name="test organization",
+            description="",
+            inteligence_organization=1,
+            organization_billing__cycle=BillingPlan.BILLING_CYCLE_MONTHLY,
+            organization_billing__payment_method=BillingPlan.PAYMENT_METHOD_CREDIT_CARD,
+            organization_billing__plan=BillingPlan.PLAN_ENTERPRISE,
+        )
+        self.project = self.organization.project.create(
+            name="project test",
+            timezone="America/Sao_Paulo",
+            flow_organization=uuid.uuid4(),
+        )
+        self.channel_flow_uuid = uuid.uuid4()
+
+    def test_create_model(self):
+        channel = Channel.objects.create(
+            project=self.project,
+            channel_flow_uuid=self.channel_flow_uuid,
+            channel_type="WA",
+            name="Test Channel"
+        )
+        same_channel = Channel.create(
+            project=self.project,
+            channel_flow_uuid=self.channel_flow_uuid,
+            channel_type="WA",
+            name="Test Channel"
+        )
+        self.assertAlmostEquals(channel.channel_flow_uuid, self.channel_flow_uuid)
+        self.assertAlmostEquals(same_channel.channel_flow_uuid, self.channel_flow_uuid)
