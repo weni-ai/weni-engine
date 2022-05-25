9671e0cb7b3ff58e9ca7191a3cebfeb5d44a466e
Jackson Barbosa <jbs@ic.ufal.br>
2022-05-25 14:32:04 -0300

    add permission class and init intelligence client

--- connect/api/v1/internal/intelligence/intelligence_rest_client.py
+++ connect/api/v1/internal/intelligence/intelligence_rest_client.py
@@ -10,15 +10,36 @@ class IntelligenceRESTClient:
         self.base_url = settings.INTELIGENCE_REST_ENDPOINT
         self.authentication_instance = InternalAuthentication()
 
-    def list_organizations(self, user_email: str):
-        result = []
+    def list_organizations(self, user_email):
 
-        request = requests.get(
+        response = requests.get(
             url=f"{self.base_url}/v2/internal/list-organizations",
             headers=self.authentication_instance.get_headers(),
             params={"user_email": user_email}
         )
 
-        print(request)
+        return response
 
-        return result
+    def get_user_organization_permission_role(self, user_email, organization_id):
+        response = requests.get(
+            url=f"{self.base_url}/v2/internal/get-user-organization-permission-role",
+            headers=self.authentication_instance.get_headers(),
+            params={"user_email": user_email, "organization_id": organization_id}
+        )
+        
+        return response
+    
+    def create_organization(self, user_email, organization_name):
+        response = requests.post(
+            url=f"{self.base_url}/v2/internal/create-organization",
+            headers.self.authentication_instance.get_headers(),
+            json=json.dumps({"user_email": user_email, "organization_name": organization_name})
+        )
+        return response
+
+    def delete_organization(self, organization_id, user_email):
+        response = requests.delete(
+            url=f"{self.base_url}/v2/internal/delete-organization",
+            headers=self.authentication_instance.get_headers(),
+            json=json.dumps({"user_email": user_email, "organization_id": organization_id})
+        )
