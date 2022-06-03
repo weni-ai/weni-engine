a328ff99e97e47c4ee8755412ef86fcb720fea62
Alisson <alissonfilipe937@gmail.com>
2022-06-03 17:56:31 -0300

    fix flake8

--- connect/api/v1/tests/test_2fa.py
+++ connect/api/v1/tests/test_2fa.py
@@ -1,14 +1,6 @@
 import json
-from operator import truediv
-from urllib import response
-import uuid as uuid4
-from unittest.mock import patch
-from django.conf import settings
-from django.http import JsonResponse
 from django.test import RequestFactory
 from django.test import TestCase
-from django.test.client import MULTIPART_CONTENT
-from requests import request
 from rest_framework import status
 
 from connect.api.v1.organization.views import (
@@ -44,7 +36,7 @@ class TwoFactorAuthTestCase(TestCase):
             user=self.user, role=OrganizationRole.ADMIN.value
         )
 
-    def request_enforce_2fa(self, organization_uuid,data, token=None):
+    def request_enforce_2fa(self, organization_uuid, data, token=None):
         authorization_header = (
             {"HTTP_AUTHORIZATION": "Token {}".format(token.key)} if token else {}
         )
@@ -59,7 +51,7 @@ class TwoFactorAuthTestCase(TestCase):
             request,
             organization_uuid=self.organization.uuid,
         )
-        
+
         content_data = json.loads(response.content)
         return response, content_data
 
@@ -68,11 +60,10 @@ class TwoFactorAuthTestCase(TestCase):
             {"HTTP_AUTHORIZATION": "Token {}".format(token.key)} if token else {}
         )
         request = self.factory.get(
-            f"v1/organization/project/?organization={organization_uuid}", **authorization_header
+            f"v1/organization/project/?organization={organization_uuid}",
+            **authorization_header,
         )
-        response = OrganizationViewSet.as_view({"get": "list"})(
-            request
-        )
+        response = OrganizationViewSet.as_view({"get": "list"})(request)
         response.render()
         content_data = json.loads(response.content)
         return (response, content_data)
@@ -82,13 +73,15 @@ class TwoFactorAuthTestCase(TestCase):
             {"HTTP_AUTHORIZATION": "Token {}".format(token.key)} if token else {}
         )
         request = self.factory.patch(
-            f"/v1/account/my-profile/set_two_factor_authentication/",
+            "/v1/account/my-profile/set_two_factor_authentication/",
             data=json.dumps(data),
             content_type="application/json",
             format="json",
             **authorization_header,
         )
-        response = MyUserProfileViewSet.as_view({"patch": "set_two_factor_authentication"})(
+        response = MyUserProfileViewSet.as_view(
+            {"patch": "set_two_factor_authentication"}
+        )(
             request,
         )
         print(response.render())
@@ -96,23 +89,16 @@ class TwoFactorAuthTestCase(TestCase):
         # return response, content_data
 
     def test_ok(self):
-        data = {
-            "2fa_required": True
-        }
+        data = {"2fa_required": True}
         # enforces 2fa for organization
         _, content_data = self.request_enforce_2fa(
-            self.organization.uuid,
-            data,
-            self.owner_token
+            self.organization.uuid, data, self.owner_token
         )
 
-        self.assertTrue(content_data['2fa_required'])
+        self.assertTrue(content_data["2fa_required"])
 
         # user cant access cause he dosen't have 2fa
         response, content_data = self.request_get_org(
-            self.organization.uuid,
-            self.user_token
+            self.organization.uuid, self.user_token
         )
         self.assertEquals(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        
