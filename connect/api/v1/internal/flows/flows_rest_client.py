2dfe1ae6518abd016cc1927f5780176d4c30f5c8
Jackson Barbosa <jbs@ic.ufal.br>
2022-07-08 21:35:36 -0300

    fix flake8 without the E902 IndentationError

--- connect/api/v1/internal/flows/flows_rest_client.py
+++ connect/api/v1/internal/flows/flows_rest_client.py
@@ -4,30 +4,26 @@ import requests
 
 from connect.api.v1.internal.internal_authentication import InternalAuthentication
 
+
 class FlowsRESTClient:
 
     def __init__(self):
         self.base_url = settings.FLOWS_REST_ENDPOINT
         self.authentication_instance = InternalAuthentication()
 
-    def create_project(
-        self,
-        project_name: str,
-        user_email: str,
-        project_timezone: str,
-    ):
+    def create_project(self, project_name: str, user_email: str, project_timezone: str):
         body = dict(
             name=project_name,
             timezone=project_timezone,
             user_email=user_email
         )
-        
+
         response = requests.post(
             url=f"{self.base_url}/",
             headers=self.authentication_instance.get_headers(),
             json=body
         )
-        
+
         return dict(status=response.status_code, data=response.data)
 
     def update_project(self, organization_uuid: int, organization_name: str):
@@ -54,14 +50,12 @@ class FlowsRESTClient:
             headers=self.authentication_instance.get_headers(),
             json=body
         )
-        
+
         return dict(status=response.status_code)
 
-    def update_user_permission_project(
-        self, organization_uuid: str, user_email: str, permission: int
-    ):
+    def update_user_permission_project(self, organization_uuid: str, user_email: str, permission: int):
         permissions = {1: "viewer", 2: "editor", 3: "administrator"}
-        
+
         body = dict(
             org_uuid=organization_uuid,
             user_email=user_email,
@@ -81,27 +75,19 @@ class FlowsRESTClient:
         body = dict(
             org_uuid=project_uuid,
             classifier_type=classifier_type,
-            is_active=is_active,    
+            is_active=is_active,
         )
-        
+
         response = requests.get(
             url=f"{self.base_url}/",
             headers=self.authentication_instance.get_headers(),
             json=body
         )
 
-        # TODO: check if response.data has a list with any element has
-        # authorization_uuid, classifier_type, name, is_active, uuid
+        # TODO: check if response.data has a list with any element has: authorization_uuid, classifier_type, name, is_active, uuid
         return dict(status=response.status_code, data=response.data)
 
-    def create_classifier(
-        self,
-        project_uuid: str,
-        user_email: str,
-        classifier_type: str,
-        classifier_name: str,
-        access_token: str,
-    ):
+    def create_classifier(self, project_uuid: str, user_email: str, classifier_type: str, classifier_name: str, access_token: str):
         body = dict(
             org=project_uuid,
             user=user_email,
