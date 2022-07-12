475f07f17540d521bbfbedfd3803ba4f5c968cb2
Jackson Barbosa <jbs@ic.ufal.br>
2022-07-12 16:02:32 -0300

    added AI rest client

---
+++ connect/api/v1/internal/intelligence/intelligence_rest_client.py
@@ -0,0 +1,104 @@
+from django.conf import settings
+
+import requests
+import json
+
+from connect.api.v1.internal.internal_authencation import InternalAuthentication
+
+
+class IntelligenceRESTClient:
+
+    def __init__(self):
+        self.base_url = settings.INTELLIGENCE_REST_ENDPOINT
+        self.authentication_instance = InternalAuthentication()
+
+    def list_organizations(self, user_email):
+        response = requests.get(
+            url=f"{self.base_url}v2/internal/organization/",
+            headers=self.authentication_instance.headers(),
+            params={"user_email": user_email}
+        )
+
+        return response.data
+
+    def get_user_organization_permission_role(self, user_email, organization_id):
+        response = requests.get(
+            url=f"{self.base_url}v2/internal/user/permission/",
+            headers=self.authentication_instance.headers(),
+            params={"user_email": user_email, "org_id": organization_id}
+        )
+        return response.data.role
+
+    def create_organization(self, user_email, organization_name):
+        response = requests.post(
+            url=f"{self.base_url}v2/internal/organization/",
+            headers=self.authentication_instance.headers(),
+            json=json.dumps({"user_email": user_email, "organization_name": organization_name})
+        )
+        return response.data
+
+    def delete_organization(self, organization_id, user_email):
+        response = requests.delete(
+            url=f"{self.base_url}v2/internal/organization/{organization_id}",
+            headers=self.authentication_instance.headers(),
+            params={"user_email": user_email}
+        )
+        return response
+
+    def update_organization(self, organization_id, organization_name, user_email):
+        response = requests.put(
+            url=f"{self.base_url}v2/internal/organization/{organization_id}",
+            headers=self.authentication_instance.headers(),
+            json=json.dumps({"name": organization_name}),
+            params={"user_email": user_email}
+        )
+        return response
+
+    def update_user_permission_organization(
+        self, organization_id, user_email, permission
+    ):
+        response = requests.put(
+            url=f"{self.base_url}v2/internal/user/permission/",
+            headers=self.authentication_instance.headers(),
+            params={"org_id": organization_id, "user_email": user_email},
+            json=json.dumps({"role": permission})
+        )
+        return response.data
+
+    def get_organization_intelligences(self, intelligence_name, organization_id):
+
+        response = requests.get(
+            url=f"{self.base_url}v2/internal/repository/",
+            headers=self.authentication_instance.headers(),
+            params={"name": intelligence_name, "org_id": organization_id}
+        )
+
+        return response.data
+
+    def update_language(self, user_email, language):
+        response = requests.put(
+            url=f"{self.base_url}v2/internal/user/language/",
+            headers=self.authentication_instance.headers(),
+            params={"user_email": user_email},
+            json=json.dumps({"language": language})
+        )
+        return response.data
+
+    def get_organization_statistics(self, organization_id, user_email):
+        response = requests.get(
+            url=f"{self.base_url}/v2/internal/organization/{organization_id}/",
+            headers=self.authentication_instance.headers(),
+            params={"user_email": user_email}
+        )
+        return len(response.data.repositories_count)
+
+    def get_count_intelligences_project(self, classifiers):
+        auth_list = set()
+        for classifier in classifiers:
+            response = requests.get(
+                url=f"{self.base_url}v2/internal/repository/retrieve_authorization/",
+                headers=self.authentication_instance.headers(),
+                params={"repository_authorization": classifier.get('authorization_uuid')}
+            )
+            auth_list.add(response.data.uuid)
+        return {"repositories_count": len(auth_list)}
