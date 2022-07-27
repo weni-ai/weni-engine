230cc2c4322fcb51cf4060fe03d75e432580746a
Jackson Barbosa <jbs@ic.ufal.br>
2022-07-27 15:05:30 -0300

    change get_headers() method to headers authentication property and run black to billing tasks

--- connect/billing/tasks.py
+++ connect/billing/tasks.py
@@ -2,7 +2,13 @@ import stripe
 import pendulum
 from connect.celery import app
 from connect.common.models import Organization, Project, BillingPlan
-from connect.billing.models import Contact, Message, SyncManagerTask, ContactCount, Channel, FailMessageLog
+from connect.billing.models import (
+    Contact,
+    Message,
+    SyncManagerTask,
+    ContactCount,
+    Channel,
+)
 from connect.elastic.flow import ElasticFlow
 from datetime import timedelta
 from django.utils import timezone
@@ -23,7 +29,9 @@ def get_messages(contact_uuid: str, before: str, after: str, project_uuid: str):
     flow_instance = utils.get_grpc_types().get("flow")
     contact = Contact.objects.get(uuid=contact_uuid)
     project = Project.objects.get(uuid=project_uuid)
-    message = flow_instance.get_message(str(project.flow_organization), str(contact.contact_flow_uuid), before, after)
+    message = flow_instance.get_message(
+        str(project.flow_organization), str(contact.contact_flow_uuid), before, after
+    )
 
     if len(message.uuid) == 0:
         return False
@@ -35,20 +43,22 @@ def get_messages(contact_uuid: str, before: str, after: str, project_uuid: str):
             text=message.text,
             created_on=message.created_on,
             direction=message.direction,
-            message_flow_uuid=message.uuid
+            message_flow_uuid=message.uuid,
         )
 
     channel = Channel.create(
         channel_type=message.channel_type,
         channel_flow_id=message.channel_id,
-        project=project
+        project=project,
     )
     contact.update_channel(channel)
     return True
 
 
 @app.task(name="sync_contacts")
-def sync_contacts(sync_before: str = None, sync_after: str = None, task_uuid: str = None):
+def sync_contacts(
+    sync_before: str = None, sync_after: str = None, task_uuid: str = None
+):
     if sync_before and sync_after:
         sync_before = pendulum.parse(sync_before)
         sync_after = pendulum.parse(sync_after)
@@ -72,26 +82,35 @@ def sync_contacts(sync_before: str = None, sync_after: str = None, task_uuid: st
         elastic_instance = ElasticFlow()
         update_fields = ["finished_at", "status"]
         for project in Project.objects.exclude(flow_id=None):
-            active_contacts = list(elastic_instance.get_contact_detailed(
-                str(project.flow_id), str(manager.before), str(manager.after)
-            ))
+            active_contacts = list(
+                elastic_instance.get_contact_detailed(
+                    str(project.flow_id), str(manager.before), str(manager.after)
+                )
+            )
             for elastic_contact in active_contacts:
                 contact = Contact.objects.create(
                     contact_flow_uuid=elastic_contact.uuid,
                     name=elastic_contact.name,
                     last_seen_on=pendulum.parse(elastic_contact.last_seen_on),
                 )
-                has_message = get_messages(str(contact.uuid), str(manager.before), str(manager.after), str(project.uuid))
+                has_message = get_messages(
+                    str(contact.uuid),
+                    str(manager.before),
+                    str(manager.after),
+                    str(project.uuid),
+                )
                 if not has_message:
                     last_message = Message.objects.filter(
                         contact=contact,
                         created_on__date__month=timezone.now().date().month,
-                        created_on__date__year=timezone.now().date().year
+                        created_on__date__year=timezone.now().date().year,
                     )
                     if not last_message.exists():
                         contact.delete()
-                        manager.fail_message.create(message="Contact don't have delivery/received message")
-                
+                        manager.fail_message.create(
+                            message="Contact don't have delivery/received message"
+                        )
+
         count_contacts.delay(manager.before, manager.after)
 
         manager.finished_at = timezone.now()
@@ -113,16 +132,14 @@ def retry_billing_tasks():
     for task in task_failed:
         task.retried = True
         task.save()
-        if task.task_type == 'count_contacts':
+        if task.task_type == "count_contacts":
             current_app.send_task(  # pragma: no cover
-                name="count_contacts",
-                args=[task.before, task.after, task.uuid]
+                name="count_contacts", args=[task.before, task.after, task.uuid]
             )
 
-        elif task.task_type == 'sync_contacts':
+        elif task.task_type == "sync_contacts":
             current_app.send_task(  # pragma: no cover
-                name="sync_contacts",
-                args=[task.before, task.after, task.uuid]
+                name="sync_contacts", args=[task.before, task.after, task.uuid]
             )
 
     return True
@@ -135,23 +152,32 @@ def count_contacts(before, after, task_uuid: str = None):
     else:
 
         manager = SyncManagerTask.objects.create(
-                task_type="count_contacts",
-                started_at=pendulum.now(),
-                before=before,
-                after=after
-            )
+            task_type="count_contacts",
+            started_at=pendulum.now(),
+            before=before,
+            after=after,
+        )
     try:
         for project in Project.objects.all():
             for channel in project.channel.all():
-                amount = Contact.objects.filter(channel=channel, last_seen_on__range=(after, before)).count()
+                amount = Contact.objects.filter(
+                    channel=channel, last_seen_on__range=(after, before)
+                ).count()
                 try:
-                    contact_count = ContactCount.objects.filter(channel=channel, created_at__range=(after, before))\
-                    .order_by("created_at").last()
+                    contact_count = (
+                        ContactCount.objects.filter(
+                            channel=channel, created_at__range=(after, before)
+                        )
+                        .order_by("created_at")
+                        .last()
+                    )
                 except ContactCount.DoesNotExist:
-                    contact_count = ContactCount.objects.create(channel=channel, count=0)
+                    contact_count = ContactCount.objects.create(
+                        channel=channel, count=0
+                    )
                 contact_count.increase_contact_count(amount)
             manager.status = True
-            manager.finished_at=pendulum.now()
+            manager.finished_at = pendulum.now()
             manager.save(update_fields=["status", "finished_at"])
             return True
     except Exception as error:
@@ -160,7 +186,6 @@ def count_contacts(before, after, task_uuid: str = None):
         manager.status = False
         manager.save(update_fields=["finished_at", "status"])
         return False
-
 
 
 @app.task(name="refund_validation_charge")
@@ -172,7 +197,9 @@ def refund_validation_charge(charge_id):  # pragma: no cover
 
 @app.task(name="problem_capture_invoice")
 def problem_capture_invoice():
-    for organization in Organization.objects.filter(organization_billing__plan=BillingPlan.PLAN_ENTERPRISE, is_suspended=False):
+    for organization in Organization.objects.filter(
+        organization_billing__plan=BillingPlan.PLAN_ENTERPRISE, is_suspended=False
+    ):
         if organization.organization_billing.problem_capture_invoice:
             organization.is_suspended = True
             organization.save(update_fields=["is_suspended"])
@@ -181,5 +208,5 @@ def problem_capture_invoice():
             for project in organization.project.all():
                 current_app.send_task(  # pragma: no cover
                     name="update_suspend_project",
-                    args=[project.flow_organization, True]
+                    args=[project.flow_organization, True],
                 )
