31f03aa35b795df960c4e004d24f92ddf64dbdbd
Alisson <alissonfilipe937@gmail.com>
2022-08-09 19:30:12 -0300

    get paginated contacts

--- connect/elastic/flow.py
+++ connect/elastic/flow.py
@@ -27,3 +27,47 @@ class ElasticFlow(ElasticHandler):
         contacts = Search(using=self.client, index=index).query(qs)
         response = contacts.scan()
         return response
+
+    def get_paginated_contacts(self, flow_id: int, before: str, after: str, scroll_id: str = None):
+        before, after = es_convert_datetime(before, after)
+
+        if scroll_id:
+            page = self.client.scroll(scroll_id=scroll_id, scroll='1m')
+            return page['hits']['hits']
+
+        query = {
+            "query": {
+                "bool":{
+                    "must": [
+                        {
+                            "match":{
+                                "org_id": f"{flow_id}"
+                            },
+                        },
+                        {
+                            "match":{
+                                "is_active": "true",
+                            }
+                        },
+                        {
+                            "range":{
+                                "last_seen_on":{
+                                    "gte": str(after),
+                                    "lte": str(before)
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        page = self.client.search(index="contacts", body=query, scroll=settings.SCROLL_KEEP_ALIVE, size=settings.SCROLL_SIZE)
+        scroll_id = page["_scroll_id"]
+        scroll_size = page["hits"]["total"]["value"]
+        hits = page["hits"]["hits"]
+        scroll = {
+            "scroll_id": scroll_id,
+            "scroll_size": scroll_size
+        }
+        return scroll, hits
