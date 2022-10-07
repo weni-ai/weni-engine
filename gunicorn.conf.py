30b575b86a14e067eda6d5b1cb5a1eaec648cc80
Jackson Barbosa <jbs@ic.ufal.br>
2022-10-06 23:28:43 -0300

    changed the workers config to default value

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 import multiprocessing
+import os
 
 bind = '0.0.0.0:80'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = os.environ.get("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2 + 1)
 worker_class = 'gevent'
 raw_env = ['DJANGO_SETTINGS_MODULE=connect.settings']
