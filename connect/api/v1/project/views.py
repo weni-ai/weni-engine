aa17edad5f9b239f64b27d1f3b1162cbb9953beb
Sandro Meireles <sandro.meirelesdev@gmail.com>
2022-10-21 11:19:41 -0300

    fix: Get project UUID by request.query_params instead request.data

--- connect/api/v1/project/views.py
+++ connect/api/v1/project/views.py
@@ -436,7 +436,7 @@ class ProjectViewSet(
         permission_classes=[ModuleHasPermission],
     )
     def list_flows(self, request, **kwargs):
-        project_uuid = request.data.get('project_uuid')
+        project_uuid = request.query_params.get('project_uuid')
         project = get_object_or_404(Project, uuid=project_uuid)
         task = tasks.list_project_flows.delay(str(project.flow_organization))
         task.wait()
