4229553906471ded56237caadc6cc4f61b7ef9b7
elitonzky <elitonzky>
2022-12-13 15:58:02 -0300

    feature:
    create viewset to list and retrieve channel types

---
+++ connect/api/v1/channel_types/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+
+from connect.api.v1.internal.flows.flows_rest_client import FlowsRESTClient
+
+from django.http import JsonResponse
+
+
+class ChannelTypesViewSet(viewsets.Viewset):
+    """ Viewset for rp-apps channels_types listing and retrieve channels types"""
+
+    @action(
+        detail=False,
+        methods=["GET"],
+        url_name='list-channel-types',
+    )
+    def list_channel_types(self, request):
+        channel_type_code = request.query_params.get('channel_type_code', None)
+        rest_client = FlowsRESTClient()
+        response = rest_client.list_channel_types(channel_type_code)
+        return JsonResponse(status=response.status_code, data=response.json())
