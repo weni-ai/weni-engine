1eeff7518888ab813ca7d9c6fe436fb29cfa96e8
elitonzky <elitonzky>
2022-12-19 12:34:33 -0300

    feature:
    add permission_classes for authentication

--- connect/api/v1/channel_types/views.py
+++ connect/api/v1/channel_types/views.py
@@ -1,13 +1,16 @@
 from rest_framework import views
 from rest_framework.decorators import action
+from rest_framework import mixins, permissions
 
 from connect.api.v1.internal.flows.flows_rest_client import FlowsRESTClient
 
 from django.http import JsonResponse
 
 class ChannelTypesAPIView(views.APIView):
-    """ Viewset for rp-apps channels_types listing and retrieve channels types"""
+    permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request):
+        """ Returns rapidpro channel listing and detail """
         channel_type_code = request.query_params.get('channel_type_code', None)
         rest_client = FlowsRESTClient()
         response = rest_client.list_channel_types(channel_type_code)
