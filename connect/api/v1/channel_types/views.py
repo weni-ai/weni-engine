f94d799dfe0e7bcce75350cf943a7a955baea67b
elitonzky <elitonzky>
2022-12-20 08:51:01 -0300

    fix: change JsonResponse to Response

--- connect/api/v1/channel_types/views.py
+++ connect/api/v1/channel_types/views.py
@@ -1,11 +1,8 @@
 from rest_framework import views
-from rest_framework.decorators import action
-from rest_framework import mixins, permissions
+from rest_framework.response import Response
 
 from connect.api.v1.internal.permissions import ModuleHasPermission
 from connect.api.v1.internal.flows.flows_rest_client import FlowsRESTClient
-
-from django.http import JsonResponse
 
 
 class ChannelTypesAPIView(views.APIView):
@@ -16,4 +13,4 @@ class ChannelTypesAPIView(views.APIView):
         channel_type_code = request.query_params.get('channel_type_code', None)
         rest_client = FlowsRESTClient()
         response = rest_client.list_channel_types(channel_type_code)
-        return JsonResponse(status=response.status_code, data=response.json())
+        return Response(status=response.status_code, data=response)
