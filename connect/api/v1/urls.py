09afc65a7eec838c1dc75a5df3851be411960ff0
Jackson Barbosa <jbs@ic.ufal.br>
2022-12-22 16:08:34 -0300

    adding recent activity model and your view with post new activities

--- connect/api/v1/urls.py
+++ connect/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from .routers import router
-
+from .recent_activity.views import RecentActivityAPIView
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("recent-activity", RecentActivityAPIView.as_view(), name="recent-activity")
 ]
