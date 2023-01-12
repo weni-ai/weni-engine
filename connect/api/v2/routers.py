f79e38c2cdd037abb498b5a7e634658da05891e2
Jackson Barbosa <jbs@ic.ufal.br>
2023-01-12 20:24:09 -0300

    adding CRUD to channels internal communication

--- connect/api/v2/routers.py
+++ connect/api/v2/routers.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
+from connect.api.v2.channels.views import ChannelsAPIView, CreateWACChannelAPIView
+
 # from connect.api.v2.organizations import views as organization_views
 # from connect.api.v2.projects import views as project_views
 # from connect.api.v2.plans import views as plan_views
 
-# router = routers.SimpleRouter()
+router = routers.SimpleRouter()
 
 # router.register('organizations', organization_views.OrganizationViewSet, basename="organizations")
 
@@ -15,8 +17,10 @@ from rest_framework_nested import routers
 # plans_router = routers.NestedSimpleRouter(router, r"organizations", lookup="plan")
 # plans_router.register('plans', plan_views.PlanViewSet, basename="organization-plans")
 
-# urlpatterns = [
-#     path("", include(router.urls)),
-#     path("", include(projects_router.urls)),
-#     path("", include(plans_router.urls)),
-# ]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("channel", ChannelsAPIView.as_view(), name="channels"),
+    path("create-wac-channel", CreateWACChannelAPIView.as_view(), name="create-wac-channel")
+    # path("", include(projects_router.urls)),
+    # path("", include(plans_router.urls)),
+]
