077ad4d878260308dc48e4e7b508973ab19e71d0
Jackson Barbosa <jbs@ic.ufal.br>
2023-01-12 22:56:16 -0300

    adding create ticketer endpoint

--- connect/api/v2/routers.py
+++ connect/api/v2/routers.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
+from connect.api.v2.ticketer.views import TicketerAPIView
+
 # from connect.api.v2.organizations import views as organization_views
 # from connect.api.v2.projects import views as project_views
 # from connect.api.v2.plans import views as plan_views
 
-# router = routers.SimpleRouter()
+router = routers.SimpleRouter()
 
 # router.register('organizations', organization_views.OrganizationViewSet, basename="organizations")
 
@@ -15,8 +17,9 @@ from rest_framework_nested import routers
 # plans_router = routers.NestedSimpleRouter(router, r"organizations", lookup="plan")
 # plans_router.register('plans', plan_views.PlanViewSet, basename="organization-plans")
 
-# urlpatterns = [
-#     path("", include(router.urls)),
-#     path("", include(projects_router.urls)),
-#     path("", include(plans_router.urls)),
-# ]
+urlpatterns = [
+    path("ticketer", TicketerAPIView.as_view(), name="ticketer"),
+    path("", include(router.urls)),
+    #     path("", include(projects_router.urls)),
+    #     path("", include(plans_router.urls)),
+]
