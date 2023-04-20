0fcbe2effddcf6fa397f0900087ad7c233ea2722
zMardone <mardone.dev@gmail.com>
2023-04-20 18:39:53 -0300

    Adding tests to template-project models

--- connect/template_projects/tests.py
+++ connect/template_projects/tests.py
@@ -1,6 +1,6 @@
 import json
 from django.test import TestCase, RequestFactory
-from connect.template_projects.models import TemplateType
+from connect.template_projects.models import TemplateType, TemplateAI, TemplateFeature
 from connect.api.v1.tests.utils import create_user_and_token
 from connect.api.v2.template_projects.views import TemplateTypeViewSet
 
@@ -50,3 +50,71 @@ class TemplateTypeViewSetTestCase(TestCase):
             id=template_id
         )
         self.assertEqual(response["name"], "name")
+
+
+class TemplateTypeModelTestCase(TestCase):
+
+    def setUp(self):
+
+        self.template_type_object = TemplateType.objects.create(
+            level=1,
+            category="category",
+            description="description",
+            name="name",
+            setup={"setup": "setup"}
+        )
+
+    def test_str_returns_valid_id(self):
+
+        model_id = self.template_type_object.id
+        self.assertIsInstance(model_id, int)
+
+
+class TemplateAIModelTestCase(TestCase):
+
+    def setUp(self):
+
+        self.template_type_object = TemplateType.objects.create(
+            level=1,
+            category="category",
+            description="description",
+            name="name",
+            setup={"setup": "setup"}
+        )
+
+        self.template_ai_object = TemplateAI.objects.create(
+            name="name",
+            description="description",
+            template_type=self.template_type_object
+        )
+
+    def test_str_returns_valid_id(self):
+
+        model_id = self.template_ai_object.id
+        self.assertIsInstance(model_id, int)
+
+
+class TemplateFeatureModelTestCase(TestCase):
+
+    def setUp(self):
+
+        self.template_type_object = TemplateType.objects.create(
+            level=1,
+            category="category",
+            description="description",
+            name="name",
+            setup={"setup": "setup"}
+        )
+
+        self.template_feature_object = TemplateFeature.objects.create(
+            name="name",
+            description="description",
+            type="type",
+            feature_identifier="feature_identifier",
+            template_type=self.template_type_object
+        )
+
+    def test_str_returns_valid_id(self):
+
+        model_id = self.template_feature_object.id
+        self.assertIsInstance(model_id, int)
