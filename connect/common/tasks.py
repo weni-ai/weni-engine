be0a84b30d17320ffeab185b72188a25cfe34db3
Jackson Barbosa <jbs@ic.ufal.br>
2023-05-22 18:18:05 -0300

    fix updated field on sync_project_information

--- connect/common/tasks.py
+++ connect/common/tasks.py
@@ -389,7 +389,7 @@ def sync_project_information():
             
             if "id" in flow_result:
                 project.flow_id = flow_result.get("id")
-                updated_fields.append("id")
+                updated_fields.append("flow_id")
             logger.info(f"project: {project.__str__()}")
             if len(updated_fields) > 0:
                 project.save(update_fields=updated_fields)
