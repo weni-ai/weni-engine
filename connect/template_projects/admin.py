cc1437eae5505ff7249559936c8bbad1f0ffcb0b
Alisson <alissonfilipe937@gmail.com>
2023-06-06 16:14:37 -0300

    Create TemplateType in django admin

--- connect/template_projects/admin.py
+++ connect/template_projects/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from connect.template_projects.models import TemplateType, TemplateFeature, TemplateFlow
 
-# Register your models here.
-admin.site.register(TemplateType)
-admin.site.register(TemplateFeature)
-admin.site.register(TemplateFlow)
+
+class TemplateFeatureInline(admin.TabularInline):
+    model = TemplateFeature.template_type.through
+    extra = 1
+
+
+class TemplateFlowInline(admin.TabularInline):
+    model = TemplateFlow
+    extra = 1
+
+
+class TemplateTypeAdmin(admin.ModelAdmin):
+    inlines = [TemplateFeatureInline, TemplateFlowInline]
+    search_fields = ["category", "level"]
+    list_display = ["name", "category"]
+
+
+class TemplateFeatureAdmin(admin.ModelAdmin):
+    fields = ("name", "description", "type", "feature_identifier")
+
+
+admin.site.register(TemplateType, TemplateTypeAdmin)
+admin.site.register(TemplateFeature, TemplateFeatureAdmin)
