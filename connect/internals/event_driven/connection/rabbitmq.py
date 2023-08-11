44bc29e7fae2eea324b968248378fc67236b9123
Jackson Barbosa <jbs@ic.ufal.br>
2023-08-11 15:00:38 -0300

    adding retries to connections to ensure messages are sent

--- connect/internals/event_driven/connection/rabbitmq.py
+++ connect/internals/event_driven/connection/rabbitmq.py
@@ -1,8 +1,11 @@
+import time
+
 from pika import BlockingConnection, ConnectionParameters, PlainCredentials
+
 from django.conf import settings
 
 
-class RabbitMQConnection:  # pragma: no cover
+class RabbitMQConnection:
     _instance = None
 
     def __new__(cls):
@@ -11,18 +14,30 @@ class RabbitMQConnection:  # pragma: no cover
             cls._instance.connect()
         return cls._instance
 
+    def make_connection(self):
+        if self.connection.is_closed:
+            self._establish_connection()
+        return self.connection.is_open
+
     def connect(self):
-        if not hasattr(self, "connection"):
-            self.connection = BlockingConnection(ConnectionParameters(
-                host=settings.EDA_BROKER_HOST,
-                port=settings.EDA_BROKER_PORT,
-                virtual_host=settings.EDA_VIRTUAL_HOST,
-                credentials=PlainCredentials(
-                    username=settings.EDA_BROKER_USER,
-                    password=settings.EDA_BROKER_PASSWORD
-                )
-            ))
-            self.channel = self.connection.channel()
+        try:
+            if not hasattr(self, "connection"):
+                self._establish_connection()
+        except Exception as e:
+            print("Error while connecting to RabbitMQ:", str(e))
+            time.sleep(5)  # Espera antes de tentar reconectar
+            self._establish_connection()
+
+    def _establish_connection(self):
+        self.connection = BlockingConnection(ConnectionParameters(
+            host=settings.EDA_BROKER_HOST,
+            port=settings.EDA_BROKER_PORT,
+            credentials=PlainCredentials(
+                username=settings.EDA_BROKER_USER,
+                password=settings.EDA_BROKER_PASSWORD
+            )
+        ))
+        self.channel = self.connection.channel()
 
     def close(self):
         if hasattr(self, "connection"):
