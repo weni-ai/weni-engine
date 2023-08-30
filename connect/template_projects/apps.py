d2ba9f95c407bc6b0e85e289c91a4463058baa46
Jackson Barbosa <jbs@ic.ufal.br>
2023-08-30 10:53:33 -0300

    adding organization id to message of project creation; add signal to app ready on template type

--- connect/template_projects/apps.py
+++ connect/template_projects/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TemplateProjectsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'connect.template_projects'
+
+    def ready(self):
+        from signals import create_template_type  # noqa: F401
