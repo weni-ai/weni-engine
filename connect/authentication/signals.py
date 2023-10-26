0f98b1cb115b3b6dbcb179585e607e90c89d4f08
Mardone <mardone.dev@gmail.com>
2023-10-26 17:26:38 -0300

    moving signal keycloak update to serializers

--- connect/authentication/signals.py
+++ connect/authentication/signals.py
@@ -3,7 +3,6 @@ import logging
 from django.conf import settings
 from django.db import models
 from django.dispatch import receiver
-from keycloak import exceptions
 
 from connect.api.v1.keycloak import KeycloakControl
 from connect.authentication.models import User
@@ -13,20 +12,6 @@ logger = logging.getLogger("connect.authentication.signals")
 
 @receiver(models.signals.post_save, sender=User)
 def signal_user(instance, created, **kwargs):
-    # if not settings.TESTING:
-    #     try:
-    #         keycloak_instance = KeycloakControl()
-
-    #         user_id = keycloak_instance.get_user_id_by_email(email=instance.email)
-    #         keycloak_instance.get_instance().update_user(
-    #             user_id=user_id,
-    #             payload={
-    #                 "firstName": instance.first_name,
-    #                 "lastName": instance.last_name,
-    #             },
-    #         )
-    #     except exceptions.KeycloakGetError as e:
-    #         logger.error(e)
 
     if created:
         from connect.common.models import (
