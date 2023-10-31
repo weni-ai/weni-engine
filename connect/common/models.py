f14c4767a8d5a68c311707c2b6b2310182cbb005
Alisson <alissonfilipe937@gmail.com>
2023-10-31 18:41:08 -0300

    alter method to count attendances

--- connect/common/models.py
+++ connect/common/models.py
@@ -1005,15 +1005,17 @@ class Project(models.Model):
         return mail
 
     def get_contacts(self, before: str, after: str, counting_method: str = None):
-        from connect.billing.models import Contact, ContactCount
+        from connect.billing.models import Contact
+        from connect.billing.utils import get_attendances
+
         if not counting_method:
             counting_method = self.organization.organization_billing.plan_method
-        
+
         if counting_method == BillingPlan.ACTIVE_CONTACTS:
             return Contact.objects.filter(project=self).filter(last_seen_on__range=(after, before)).distinct("contact_flow_uuid").count()
 
-        contacts_day_count = ContactCount.objects.filter(project=self, day__range=(after, before))
-        total = sum([day_count.count for day_count in contacts_day_count])
+        total = get_attendances(self, after, before)
+
         return total
 
 
