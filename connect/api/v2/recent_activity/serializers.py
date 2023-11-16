e5c8d4bfa0e222077184171a5172c9cd3ba9a8a3
Mardone <mardone.dev@gmail.com>
2023-11-16 14:41:36 -0300

    Fix: List serializer pagination

--- connect/api/v2/recent_activity/serializers.py
+++ connect/api/v2/recent_activity/serializers.py
@@ -4,29 +4,11 @@ from connect.common.models import Project, RecentActivity
 from connect.authentication.models import User
 
 
-class RecentActivityListSerializer(serializers.ListSerializer):
-    def to_representation(self, data):
-        request = self.context.get('request')
-        project_uuid = request.query_params.get("project")
-
-        try:
-            project = Project.objects.get(uuid=project_uuid)
-        except Project.DoesNotExist:
-            raise serializers.ValidationError({"message": "Project does not exist."})
-
-        if not project.project_authorizations.filter(user__email=request.user.email).exists():
-            raise serializers.ValidationError({"message": "Permission denied."})
-
-        recent_activities = RecentActivity.objects.filter(project__uuid=project_uuid).order_by("-created_on")
-        return super().to_representation(recent_activities)
-
-
 class RecentActivitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = RecentActivity
         fields = ['user', 'action', 'entity', 'entity_name', 'intelligence_id', 'flow_organization', 'project_uuid', 'project']
-        list_serializer_class = RecentActivityListSerializer
 
     user = serializers.SlugRelatedField(slug_field='email', queryset=User.objects.all(), required=False)
     project = serializers.SlugRelatedField(slug_field='uuid', queryset=Project.objects.all(), required=False)
