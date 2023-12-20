c82191242f0fb51ebabbd5725b1d748134ca5f06
Mardone <mardone.dev@gmail.com>
2023-12-20 16:39:15 -0300

    Change: Make flows endpoint call async

--- connect/authentication/tasks.py
+++ connect/authentication/tasks.py
@@ -8,10 +8,11 @@ logger = logging.getLogger(__name__)
 
 @app.task(name="send_user_flow_info", ignore_result=True)
 def send_user_flow_info(
-    flow_data
+    flow_data: dict,
+    user: User,
 ) -> bool:
     try:
-        User().send_request_flow_user_info(flow_data=flow_data)
+        user.send_request_flow_user_info(flow_data=flow_data)
         return True
     except Exception as e:
         logger.error(e)
