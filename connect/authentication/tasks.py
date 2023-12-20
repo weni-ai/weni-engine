5d0c90c97ac19ab929c95985af66818a450af260
Mardone <mardone.dev@gmail.com>
2023-12-20 16:28:08 -0300

    Add: Tasks.py authentication file

---
+++ connect/authentication/tasks.py
@@ -0,0 +1,18 @@
+import logging
+
+from connect.celery import app
+from connect.authentication.models import User
+
+logger = logging.getLogger(__name__)
+
+
+@app.task(name="send_user_flow_info", ignore_result=True)
+def send_user_flow_info(
+    flow_data
+) -> bool:
+    try:
+        User().send_request_flow_user_info(flow_data=flow_data)
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False
