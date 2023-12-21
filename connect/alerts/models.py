f064673b32c8b81b3596984b1777995f6accedb1
Mardone <mardone.dev@gmail.com>
2023-12-21 17:33:01 -0300

    Add: Alert models

--- connect/alerts/models.py
+++ connect/alerts/models.py
@@ -1,3 +1,19 @@
+import uuid as uuid4
 from django.db import models
 
-# Create your models here.
+
+class Alert(models.Model):
+
+    alert_types = [
+        (1, "very_high"),
+        (2, "high"),
+        (3, "medium"),
+        (4, "low"),
+        (5, "very_low"),
+    ]
+
+    can_be_closed = models.BooleanField()
+    text = models.TextField()
+    type = models.IntegerField(choices=alert_types)
+    uuid = models.UUIDField(default=uuid4.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
