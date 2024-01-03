1d33a87f372d2e45d54ed3f3ea92c408a813aef3
Mardone <mardone.dev@gmail.com>
2024-01-03 15:44:05 -0300

    Remove: Test_update_flows_project, not made by syncronous call anymore

--- connect/api/v2/projects/tests.py
+++ connect/api/v2/projects/tests.py
@@ -466,30 +466,6 @@ class ProjectViewSetTestCase(TestCase):
 
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
-    @patch("connect.api.v1.internal.flows.flows_rest_client.FlowsRESTClient.update_project")
-    def test_update_flows_project(self, mock_update_project):
-
-        mock_response = Mock()
-        mock_response.status_code = status.HTTP_200_OK
-        mock_response.json.return_value = {"message": "Success"}
-
-        mock_update_project.return_value = mock_response
-
-        organization_uuid = str(self.org_1.uuid)
-        project_uuid = str(self.project1.uuid)
-
-        url = f"v2/organizations/{organization_uuid}/projects/{project_uuid}/"
-        data = {
-            "name": "Novo nome",
-            "timezone": "America/New_York",
-            "date_format": "D",
-        }
-        method = {"patch": "partial_update"}
-        response, content_data = self.request(url, method, data, user=self.user, pk=organization_uuid, project_uuid=project_uuid)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(content_data.get("name"), data.get("name"))
-        mock_update_project.assert_called_once_with(project_uuid, name="Novo nome", timezone="America/New_York", date_format="D")
-
 
 class ProjectTestCase(TestCase):
 
