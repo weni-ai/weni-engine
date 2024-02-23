7a7c4e710474fb9c3460dfd288666e4928b2a3ce
Mardone <mardone.dev@gmail.com>
2024-02-23 14:41:44 -0300

    Fix: Wrong management directory

--- connect/internals/event_driven/connection/pymqp.py
+++ connect/internals/event_driven/connection/pymqp.py
@@ -1,8 +1,41 @@
 import time
-
 import amqp
 
-from .rabbitmq import RabbitMQConnection
+from django.conf import settings
+
+
+class RabbitMQPymqpConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(RabbitMQPymqpConnection, cls).__new__(cls)
+            cls._instance.connect()
+        return cls._instance
+
+    def _establish_connection(self):
+        self.connection = amqp.Connection(
+            host=settings.EDA_BROKER_HOST,
+            virtual_host=settings.EDA_VIRTUAL_HOST,
+            userid=settings.EDA_BROKER_USER,
+            password=settings.EDA_BROKER_PASSWORD,
+            port=settings.EDA_BROKER_PORT
+        )
+        self.channel = self.connection.channel()
+
+    def connect(self):
+        try:
+            if not hasattr(self, "connection"):
+                self._establish_connection()
+        except Exception as e:
+            print("Error while connecting to RabbitMQ:", str(e))
+            time.sleep(5)  # Wait until try to reconnect
+            self._establish_connection()
+
+    def make_connection(self):
+        if self.connection.is_closing:
+            self._establish_connection()
+        return self.connection.is_alive
 
 
 class PyAMQPConnectionBackend:
@@ -10,7 +43,7 @@ class PyAMQPConnectionBackend:
 
     def __init__(self, handle_consumers: callable):
         self._handle_consumers = handle_consumers
-        self.rabbitmq_instance = RabbitMQConnection()
+        self.rabbitmq_instance = RabbitMQPymqpConnection()
 
     def _drain_events(self, connection: amqp.connection.Connection):
         while True:
