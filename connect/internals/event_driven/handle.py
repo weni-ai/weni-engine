3964191c6b97a54507bd507a71cb7c1583a70c7f
Eric Costa <ericosta.dev@gmail.com>
2024-05-02 15:09:50 -0300

    feat: adding new consume to recent-activity.nexus

--- connect/internals/event_driven/handle.py
+++ connect/internals/event_driven/handle.py
@@ -4,11 +4,12 @@ from .consumer.recent_activities import RecentActivitiesConsumer
 
 
 # Event driven using rabbitmq handles consumers
-def handle_consumers(
-        channel: Channel
-) -> None:
+def handle_consumers(channel: Channel) -> None:
 
     channel.basic_consume(
-        "recent-activity.projects",
-        callback=RecentActivitiesConsumer().handle
+        "recent-activity.projects", callback=RecentActivitiesConsumer().handle
+    )
+
+    channel.basic_consume(
+        "recent-activity.nexus", callback=RecentActivitiesConsumer().handle
     )
