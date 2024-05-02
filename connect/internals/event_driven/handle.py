d05056d71e9ccc515f4e1caffd6ed0bccc360c56
Eric Costa <ericosta.dev@gmail.com>
2024-05-02 17:19:22 -0300

    fix: renaming connect queue

--- connect/internals/event_driven/handle.py
+++ connect/internals/event_driven/handle.py
@@ -7,5 +7,5 @@ from .consumer.recent_activities import RecentActivitiesConsumer
 def handle_consumers(channel: Channel) -> None:
 
     channel.basic_consume(
-        "recent-activity.projects", callback=RecentActivitiesConsumer().handle
+        "recent-activity.connect", callback=RecentActivitiesConsumer().handle
     )
