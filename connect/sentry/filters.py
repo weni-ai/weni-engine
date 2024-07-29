402973e2c035eb0257fdb281712ed66a80283252
Eric Costa <ericosta.dev@gmail.com>
2024-07-29 11:41:42 -0300

    fix: updating type of return type

--- connect/sentry/filters.py
+++ connect/sentry/filters.py
@@ -1,7 +1,7 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
-def filter_events(event: Dict, hint: Dict, events_to_filter: List[str]) -> Dict | None:
+def filter_events(event: Dict, hint: Dict, events_to_filter: List[str]) -> Optional[Dict]:
     try:
         event_type: str | None = event.get("exception").get("values")[0].get("type")
         if event_type in events_to_filter:
