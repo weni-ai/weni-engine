897ba5d9c1dc70786dd71a654610ec594732378b
Jackson Barbosa <=>
2025-01-09 12:26:31 -0300

    adding a endpoint to check if exists the project and create a new user when the user request not exists in project; just create a user when he not exists in platform/keycloak

--- connect/api/v2/commerce/views.py
+++ connect/api/v2/commerce/views.py
@@ -1,11 +1,15 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import views
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin
 from connect.api.v2.commerce.permissions import CanCommunicateInternally
 from connect.api.v2.commerce.serializers import CommerceSerializer
 from connect.api.v2.paginations import CustomCursorPagination
-from connect.common.models import Organization
+from connect.common.models import Organization, Project, ProjectAuthorization, OrganizationRole
+from connect.authentication.models import User
+from connect.usecases.users.create import CreateKeycloakUserUseCase
+from connect.usecases.users.user_dto import KeycloakUserDTO
 
 
 class CommerceOrganizationViewSet(CreateModelMixin, GenericViewSet):
@@ -27,3 +31,51 @@ class CommerceOrganizationViewSet(CreateModelMixin, GenericViewSet):
         }
 
         return Response(response, status=status.HTTP_201_CREATED)
+
+
+class CommerceProjectCheckExists(views.APIView):
+
+    permission_classes = [CanCommunicateInternally]
+
+    def get(self, request):
+        data = request.data
+        project = Project.objects.filter(vtex_account=data.get("vtex_account"))
+
+        if project.count() > 0:
+            project = project.first()
+        else:
+            return Response(
+                {
+                    "message": f"Project with vtex_account {data.get('vtex_account')} doesn't exists!",
+                    "data": {
+                        "has_project": False
+                    }
+                }, status=status.HTTP_200_OK)
+
+        organization = project.organization
+        permission = ProjectAuthorization.objects.filter(project=project, user__email=data.get("user_email"))
+
+        if permission.count() > 0:
+            permission = permission.first()
+        else:
+            try:
+                user = User.objects.get(email=data.get("user_email"))
+            except Exception:
+                user_dto = KeycloakUserDTO(
+                    email=data.get("user_email"),
+                    company_name=project.organization.name,
+                )
+                create_keycloak_user_use_case = CreateKeycloakUserUseCase(user_dto)
+                user_info = create_keycloak_user_use_case.execute()
+                user = user_info.get("user")
+            organization.authorizations.create(
+                user=user, role=OrganizationRole.ADMIN.value
+            )
+        return Response(
+            {
+                "message": f"Project {project.name} exists and user {permission.user.email} has permission",
+                "data": {
+                    "project_uuid": project.uuid,
+                    "has_project": True
+                }
+            }, status=status.HTTP_200_OK)
