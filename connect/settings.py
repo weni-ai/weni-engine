c0ae6462e624b88c7693662676d3c98fd193f7f2
Eric Costa <ericosta.dev@gmail.com>
2025-01-24 09:34:56 -0300

    fix: set default values for Keycloak database configuration in settings.py

--- connect/settings.py
+++ connect/settings.py
@@ -565,10 +565,10 @@ if ALLOW_CRM_ACCESS:
 
 USE_EDA_PERMISSIONS = env.bool("USE_EDA_PERMISSIONS", default=True)
 
-KC_DB_NAME = env.str("KC_DB_NAME")
-KC_DB_USER = env.str("KC_DB_USER")
-KC_DB_PASSWORD = env.str("KC_DB_PASSWORD")
-KC_DB_HOST = env.str("KC_DB_HOST")
-KC_DB_PORT = env.int("KC_DB_PORT")
+KC_DB_NAME = env.str("KC_DB_NAME", default="")
+KC_DB_USER = env.str("KC_DB_USER", default="")
+KC_DB_PASSWORD = env.str("KC_DB_PASSWORD", default="")
+KC_DB_HOST = env.str("KC_DB_HOST", default="")
+KC_DB_PORT = env.int("KC_DB_PORT", default=0)
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = env.int("DATA_UPLOAD_MAX_NUMBER_FIELDS", default=10000)
