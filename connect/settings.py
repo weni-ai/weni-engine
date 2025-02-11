c7e0f25313886df0e2a6234c06fd2fc39c019c7a
Marcello Alexandre <marcello.alexandre@weni.ai>
2025-02-11 10:01:58 -0300

    Fix typo add middleware django-prometheus after

--- connect/settings.py
+++ connect/settings.py
@@ -170,6 +170,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = "connect.urls"
