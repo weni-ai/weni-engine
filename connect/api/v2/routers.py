78649f6589b9917ddeb469112d5b5939b9db6709
sandro.meireles <sandro.meirelesdev@gmail.com>
2025-03-03 11:37:50 -0300

    Add project authorization endpoint for retrieving user roles

--- connect/api/v2/routers.py
+++ connect/api/v2/routers.py
@@ -31,7 +31,7 @@ from connect.api.v2.commerce.views import CommerceOrganizationViewSet, CommerceP
 from connect.api.v2.organizations import views as organization_views
 from connect.api.v2.projects import views as project_views
 from connect.api.v2.internals import views as connect_internal_views
-from connect.api.v2.auth.views import KeycloakAuthView
+from connect.api.v2.auth.views import KeycloakAuthView, ProjectAuthView
 
 router = routers.SimpleRouter()
 router.register(
@@ -118,6 +118,7 @@ urlpatterns = [
         UserAPIToken.as_view(),
         name="user-api-token",
     ),
+    path("projects/<project_uuid>/authorization", ProjectAuthView.as_view(), name="project-authorizations"),
     path("account/user-is-paying", UserIsPaying.as_view(), name="user-is-paying"),
     path("omie/accounts", OmieAccountAPIView.as_view(), name="omie-accounts"),
     path("omie/origins", OmieOriginAPIView.as_view(), name="omie-origins"),
