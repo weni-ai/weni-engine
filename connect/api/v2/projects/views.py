0df6e357ac6f4df4c8322ddf7554550c73d6b834
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2025-03-17 08:52:49 -0300

    refactor: Remove unused import of ValidationError in project views

--- connect/api/v2/projects/views.py
+++ connect/api/v2/projects/views.py
@@ -1,5 +1,4 @@
 from rest_framework import mixins, status
-from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
