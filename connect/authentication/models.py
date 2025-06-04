566f691de4cd2eafe07918ed36299632388a8fac
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-06-04 12:40:59 -0300

    Enhance password change email notifications with improved messaging and styling. Update email templates for better user experience, including personalized greetings and additional security reminders. Update translations for consistency across languages.

--- connect/authentication/models.py
+++ connect/authentication/models.py
@@ -10,6 +10,7 @@ from django.core.mail import send_mail
 from django.db import models
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
+from django.utils import translation
 
 from connect.storages import AvatarUserMediaStorage
 
@@ -177,15 +178,17 @@ class User(AbstractBaseUser, PermissionsMixin):
         if not settings.SEND_EMAILS:
             return False  # pragma: no cover
         context = {"name": self.first_name}
-        send_mail(
-            _("Password changed"),
-            render_to_string("authentication/emails/change_password.txt"),
-            None,
-            [self.email],
-            html_message=render_to_string(
-                "authentication/emails/change_password.html", context
-            ),
-        )
+
+        with translation.override(self.language):
+            send_mail(
+                _("Your password has been changed"),
+                render_to_string("authentication/emails/change_password.txt"),
+                None,
+                [self.email],
+                html_message=render_to_string(
+                    "authentication/emails/change_password.html", context
+                ),
+            )
 
     def send_email_nickname_changed(self, before_nickname: str, new_nickname: str):
         if not settings.SEND_EMAILS:
