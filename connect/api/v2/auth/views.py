5e7f1b406797efafe538d69044a7dadcb7bd4556
sandro.meireles <sandro.meirelesdev@gmail.com>
2025-07-04 17:37:00 -0300

    refactor: Add user pararm and internal authentication to ProjectAuthView

--- connect/api/v2/auth/views.py
+++ connect/api/v2/auth/views.py
@@ -1,10 +1,16 @@
-from rest_framework import views, status
-from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework import status, views
+from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 from connect.api.v2.auth.serializers import KeycloakAuthSerializer
-from connect.usecases.keycloak.authenticate import KeycloakAuthenticateUseCase
-from connect.middleware import WeniOIDCAuthentication
 from connect.common.models import ProjectAuthorization
+from connect.middleware import WeniOIDCAuthentication
+from connect.usecases.keycloak.authenticate import KeycloakAuthenticateUseCase
+
+User = get_user_model()
 
 
 class KeycloakAuthView(views.APIView):
@@ -35,19 +41,31 @@ class ProjectAuthView(views.APIView):
     def get_available_roles(self):
         return {choice[0]: choice[1] for choice in ProjectAuthorization.ROLE_CHOICES}
 
-    def get(self, request, project_uuid: str = None):
+    def get(self, request: Request, project_uuid: str = None):
+        user_email = request.query_params.get("user")
+
+        user = request.user
+
+        if user_email is not None:
+            if not request.user.has_perm("authentication.can_communicate_internally"):
+                raise PermissionDenied(
+                    "You do not have permission to access other users' data"
+                )
+
+            user = User.objects.filter(email=user_email).first()
+
+            if user is None:
+                raise NotFound("User not found")
+
         try:
-            project_authorization = request.user.project_authorizations_user.get(
+            project_authorization = user.project_authorizations_user.get(
                 project__uuid=project_uuid
             )
         except ProjectAuthorization.DoesNotExist:
-            return Response(
-                {"error": "Project authorization not found"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+            raise NotFound("Project authorization not found")
 
         response = {
-            "user": request.user.email,
+            "user": user.email,
             "project_authorization": project_authorization.role,
             "available_roles": self.get_available_roles(),
         }
