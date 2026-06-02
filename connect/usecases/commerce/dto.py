6ed575aac38e70ae18e0b03c68ab06cb54690fa1
Paulo Bernardo <paulo.bernardo@vtex.com>
2026-06-02 15:15:45 -0300

    feat: Implement Send Data Export Email functionality
    - Added SendDataExportEmailView to handle email dispatch for data exports.
    - Created SendDataExportEmailSerializer for validating email request data.
    - Developed SendDataExportEmailUseCase to manage email sending logic.
    - Introduced email templates for both HTML and plain text formats.
    - Added tests for SendDataExportEmailUseCase to ensure correct functionality and context handling.
    - Updated routing to include the new email sending endpoint.

--- connect/usecases/commerce/dto.py
+++ connect/usecases/commerce/dto.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from datetime import date
+from typing import Optional
 
 
 @dataclass(frozen=True)
@@ -8,3 +10,14 @@ class CreateVtexProjectDTO:
     language: str
     organization_name: str
     project_name: str
+
+
+@dataclass(frozen=True)
+class SendDataExportEmailDTO:
+    user_email: str
+    file_url: str
+    start_date: date
+    end_date: date
+    template: str
+    status: str
+    language: Optional[str] = None
